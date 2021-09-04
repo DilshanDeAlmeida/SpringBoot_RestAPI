2518632ce8e3678c077356039412d90ea84839cf
DilshanDeAlmeida <dilshandealmeida2@gmail.com>
2021-09-04 23:13:27 +0530

    student class implemented

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,21 +1,31 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	@GetMapping
-	public String hello(){
-		return "Hello World";
-	}
+    @GetMapping
+    public List<Student> hello() {
+        return Arrays.asList(
+                new Student(
+                        1L, "Maria", "maria.jamal@gmail.com",
+                        LocalDate.of(2000, Month.JANUARY, 5), 21)
+        );
+    }
 
 }
